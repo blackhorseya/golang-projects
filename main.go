064b9b80afc9b200cdd6572b2808812eb9c965fa
Sean Zheng <blackhorseya@gmail.com>
2023-03-13 07:35:07 +0800

    feat: add handle message

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 var (
@@ -15,5 +21,34 @@ func init() {
 }
 
 func main() {
-	fmt.Println(token)
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatalln("error creating discord session, ", err)
+	}
+
+	s.AddHandler(handleMessage)
+
+	s.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = s.Open()
+	if err != nil {
+		log.Fatalln("error opening connection, ", err)
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-signals
+
+	s.Close()
+}
+
+func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
 }
